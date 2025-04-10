d66ab8f0592cb7a30ccb9da9cddfe9599849c58d
JunYoung <ghddns34@gmail.com>
2025-04-10 15:08:39 +0900

    add/#1: 프로젝트 생성

---
+++ MLS/MLS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLS
+//
+//  Created by SeoJunYoung on 4/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
