8b5798819a7291a76f97f99ab7b191eeb53b4f3c
JunYoung <ghddns34@gmail.com>
2025-04-10 16:20:50 +0900

    chore/#3: presentation, domain, data 레이어 프레임워크로 분리 및 생성

---
+++ MLS/MLSPresentation/MLSPresentation/MLSPresentation.h
@@ -0,0 +1,18 @@
+//
+//  MLSPresentation.h
+//  MLSPresentation
+//
+//  Created by SeoJunYoung on 4/10/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLSPresentation.
+FOUNDATION_EXPORT double MLSPresentationVersionNumber;
+
+//! Project version string for MLSPresentation.
+FOUNDATION_EXPORT const unsigned char MLSPresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLSPresentation/PublicHeader.h>
+
+
