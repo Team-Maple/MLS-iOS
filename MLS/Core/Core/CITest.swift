036d82e472d538f0be9adad65b394273e05a41ec
JunYoung <ghddns34@gmail.com>
2025-04-28 14:43:09 +0900

    test/#26: CI 테스트

--- MLS/Core/Core/CITest.swift
+++ MLS/Core/Core/CITest.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class CITest {
 
+import UIKit
+
+class CITest {
+    var some: String = "어쩌구 저쩌구"
+    
+    
+    func someSomesomesomeSomesomeSomesomeSomesomeSomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSomesomeSome() {
+        
+    }
 }
