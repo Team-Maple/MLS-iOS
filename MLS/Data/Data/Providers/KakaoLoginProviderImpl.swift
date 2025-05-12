bb352708c1107d032549a89e2ed64f1ae4ec880d
p2glet <vviian.2@gmail.com>
2025-05-12 19:52:06 +0900

    feat/#44: KakaoLoginProvider 구현체 생성

--- MLS/Data/Data/Providers/KakaoLoginProviderImpl.swift
+++ MLS/Data/Data/Providers/KakaoLoginProviderImpl.swift
@@ -1,16 +1,71 @@
+import Foundation
+
 import DomainInterface
 
+import KakaoSDKAuth
+import KakaoSDKUser
 import RxSwift
 
-class KakaoLoginProviderImpl: SocialAuthenticatableProvider {
+public final class KakaoLoginProviderImpl: SocialAuthenticatableProvider {
     public struct Credential: Encodable {
-        var idToken: String?
-        var authorizationCode: String?
+        var accessToken: String?
+        var email: String?
     }
-    
-    private let authServiceResponse = PublishSubject<Credential>()
-    
-    func getCredential() -> RxSwift.Observable<Credential> {
-        return authServiceResponse
+
+    public init() {}
+
+    public func getCredential() -> Observable<Credential> {
+        let observable = PublishSubject<Credential>()
+
+        if UserApi.isKakaoTalkLoginAvailable() {
+            // 카카오톡 설치되어있을때
+            UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+                self.fetchEmail(oauthToken: oauthToken, error: error, observer: observable.asObserver())
+            }
+        } else {
+            // 카카오톡이 설치되어있지않을때
+            UserApi.shared.loginWithKakaoAccount { oauthToken, error in
+                self.fetchEmail(oauthToken: oauthToken, error: error, observer: observable.asObserver())
+            }
+        }
+
+        return observable
+        
+//        return Observable.create { [weak self] observer in
+//            if UserApi.isKakaoTalkLoginAvailable() {
+//                // 카카오톡 설치되어있을때
+//                UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+//                    self?.fetchEmail(oauthToken: oauthToken, error: error, observer: observer)
+//                }
+//            } else {
+//                // 카카오톡이 설치되어있지않을때
+//                UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+//                    self?.fetchEmail(oauthToken: oauthToken, error: error, observer: observer)
+//                }
+//            }
+//            return Disposables.create()
+//        }
+    }
+
+    private func fetchEmail(oauthToken: OAuthToken?, error: Error?, observer: AnyObserver<Credential>) {
+        if let error = error {
+            observer.onError(error)
+            return
+        }
+
+        guard let accessToken = oauthToken?.accessToken else {
+            observer.onError(AuthError.unknown(message: "토큰이 없어요"))
+            return
+        }
+
+        UserApi.shared.me { user, error in
+            if let error = error {
+                observer.onError(error)
+            } else {
+                let credential = Credential(accessToken: accessToken, email: user?.kakaoAccount?.email)
+                observer.onNext(credential)
+                observer.onCompleted()
+            }
+        }
     }
 }
--- MLS/MLS/Application/ViewController.swift
+++ MLS/MLS/Application/ViewController.swift
@@ -1,11 +1,58 @@
-//
-//  ViewController.swift
-//  MLS
-//
-//  Created by SeoJunYoung on 4/10/25.
-//
-
 import UIKit
 
+import Data
+
+import RxSwift
+import RxCocoa
+
 class ViewController: UIViewController {
+    let disposeBag = DisposeBag()
+    
+    let button: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .red
+        btn.setTitle("Kakao Login", for: .normal)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        return btn
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        bindActions()
+    }
+
+    private func setupUI() {
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            button.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+
+    private func bindActions() {
+//        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
+        button.rx.tap
+            .withUnretained(self)
+            .subscribe { owner, _ in
+                let kakaoLoginProvider = KakaoLoginProviderImpl()
+                kakaoLoginProvider.getCredential()
+                    .subscribe(onNext: { credential in
+                        print("Credential received: \(credential)")
+                    }, onError: { error in
+                        print("Login error: \(error)")
+                    }, onDisposed: {
+                        print("Subscription disposed")
+                    })
+                    .disposed(by: owner.disposeBag)
+            }
+            .disposed(by: disposeBag)
+    }
+
+    @objc private func handleLogin() {
+            print("handleLogin called")
+            
+        }
 }
