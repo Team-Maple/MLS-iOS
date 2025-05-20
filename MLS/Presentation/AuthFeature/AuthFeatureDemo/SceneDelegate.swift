d4d151d8735674fc2a7c52cf38a37a94eb74b83a
p2glet <vviian.2@gmail.com>
2025-05-20 21:36:33 +0900

    feat/#77: Input 페이지 UI 구현 및 InputBox 컴포넌트 수정

--- MLS/Presentation/AuthFeature/AuthFeatureDemo/SceneDelegate.swift
+++ MLS/Presentation/AuthFeature/AuthFeatureDemo/SceneDelegate.swift
@@ -3,31 +3,32 @@ import UIKit
 import AuthFeature
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let startVC = TermsAgreementViewController()
-        startVC.reactor = TermsAgreementReactor()
+//        let startVC = TermsAgreementViewController()
+//        startVC.reactor = TermsAgreementReactor()
+//        guard let windowScene = (scene as? UIWindowScene) else { return }
+//        window = UIWindow(windowScene: windowScene)
+//        window?.rootViewController = UINavigationController(rootViewController: startVC)
+//        window?.makeKeyAndVisible()
+
+//        let startVC = onBoardingQuestionViewController()
+        let startVC = OnBoardingInputViewController()
+        let navController = CustomNavigationController(rootViewController: startVC)
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: startVC)
+        window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
+    func sceneDidDisconnect(_ scene: UIScene) {}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
+    func sceneDidBecomeActive(_ scene: UIScene) {}
 
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
+    func sceneWillResignActive(_ scene: UIScene) {}
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
+    func sceneWillEnterForeground(_ scene: UIScene) {}
 
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
+    func sceneDidEnterBackground(_ scene: UIScene) {}
 }
-
