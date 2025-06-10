bae2eaf52528ea3f02e6c754aeddee1c41fed713
JunYoung <ghddns34@gmail.com>
2025-06-10 15:50:12 +0900

    add/#125: 모듈 타켓 추가

---
+++ MLS/Presentation/DictionaryFeature/DictionaryFeatureDemo/DictionaryFeatureDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DictionaryFeatureDemoApp.swift
+//  DictionaryFeatureDemo
+//
+//  Created by SeoJunYoung on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DictionaryFeatureDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MLS/Presentation/DictionaryFeature/DictionaryFeatureInterface/TempFile.swift
@@ -0,0 +1,8 @@
+//
+//  TempFile.swift
+//  DictionaryFeatureInterface
+//
+//  Created by SeoJunYoung on 6/10/25.
+//
+
+import Foundation
