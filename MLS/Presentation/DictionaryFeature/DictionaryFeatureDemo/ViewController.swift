22cc9afdf2d8c4b2f40e1f6a0d823a1458fb62dd
JunYoung <ghddns34@gmail.com>
2025-06-10 15:53:13 +0900

    chore/#125: demo target 추가

---
+++ MLS/Presentation/DictionaryFeature/DictionaryFeatureDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionaryFeatureDemo
+//
+//  Created by SeoJunYoung on 6/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
