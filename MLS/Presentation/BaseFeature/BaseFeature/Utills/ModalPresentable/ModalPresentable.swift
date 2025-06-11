c1941a0fbd281650c6bc77335a1617549be5221d
JunYoung <ghddns34@gmail.com>
2025-06-11 15:34:37 +0900

    fix/#125: custom modal 수정 스타일 제거

--- MLS/Presentation/BaseFeature/BaseFeature/Utills/ModalPresentable/ModalPresentable.swift
+++ MLS/Presentation/BaseFeature/BaseFeature/Utills/ModalPresentable/ModalPresentable.swift
@@ -2,22 +2,6 @@ import UIKit
 
 public protocol ModalPresentable {
     var modalHeight: CGFloat? { get }
-    var modalStyle: ModalStyle { get }
-}
-
-public enum ModalStyle {
-    case bottomSheet
-    case modal
-    case alert
-
-    var containerCornerRadius: CGFloat {
-        switch self {
-        case .bottomSheet, .modal:
-            return 20
-        case .alert:
-            return 24
-        }
-    }
 }
 
 // 모달 구성 관련 상수 정의
@@ -26,6 +10,7 @@ internal enum ModalConfig {
     static let containerBottomInset: CGFloat = 8
     static let containerHorizontalInset: CGFloat = 8
     static let containerVerticalContentInset: CGFloat = 16
+    static let containerCornerRadius: CGFloat = 20
     static let bottomSheetStyleBottomInset: CGFloat = 34
     static let bottomSheetStyleHorizontalInset: CGFloat = 20
     static let alertSheetStyleInset: CGFloat = 20
--- MLS/Presentation/BaseFeature/BaseFeature/Utills/ModalPresentable/ModalWrapperView.swift
+++ MLS/Presentation/BaseFeature/BaseFeature/Utills/ModalPresentable/ModalWrapperView.swift
@@ -2,15 +2,17 @@ import UIKit
 
 import DesignSystem
 
+import SnapKit
+
 final class ModalWrapperView: UIView {
 
     weak var parentViewController: UIViewController?
 
     let dimView = UIView()
     let containerView = UIView()
-    let gestureBar = UIView()
 
     private var initialY: CGFloat = 0
+    private var containerBottomConstraint: Constraint?
 
     init(contentViewController: UIViewController & ModalPresentable, parent: UIViewController) {
         super.init(frame: .zero)
@@ -30,98 +32,38 @@ final class ModalWrapperView: UIView {
 
         // 모달 컨테이너
         containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = contentViewController.modalStyle.containerCornerRadius
+        containerView.layer.cornerRadius = ModalConfig.containerCornerRadius
         containerView.clipsToBounds = true
         containerView.transform = CGAffineTransform(translationX: 0, y: ModalConfig.containerTransformY)
         addSubview(containerView)
 
         containerView.snp.makeConstraints { make in
-            switch contentViewController.modalStyle {
-            case .bottomSheet:
-                make.horizontalEdges.bottom.equalToSuperview()
-            case .modal:
-                make.bottom.equalTo(safeAreaLayoutGuide).inset(ModalConfig.containerBottomInset)
-                make.horizontalEdges.equalToSuperview().inset(ModalConfig.containerHorizontalInset)
-            case .alert:
-                make.centerY.equalToSuperview()
-                make.horizontalEdges.equalToSuperview().inset(ModalConfig.containerHorizontalInset)
-            }
-        }
-
-        gestureBar.backgroundColor = .neutral200
-        gestureBar.layer.cornerRadius = ModalConfig.gestureBarHeight / 2
-        gestureBar.clipsToBounds = true
-        containerView.addSubview(gestureBar)
-        switch contentViewController.modalStyle {
-        case .modal:
-            gestureBar.snp.makeConstraints { make in
-                make.top.equalToSuperview().inset(ModalConfig.gestureBarTopInset)
-                make.height.equalTo(ModalConfig.gestureBarHeight)
-                make.width.equalTo(ModalConfig.gestureBarWidth)
-                make.centerX.equalToSuperview()
-            }
-
-            let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-            containerView.addGestureRecognizer(panGesture)
-        case .bottomSheet, .alert:
-            break
+            self.containerBottomConstraint = make.bottom.equalToSuperview().constraint
+            make.horizontalEdges.equalToSuperview()
         }
 
         // 자식 뷰컨트롤러 embed
         parent.addChild(contentViewController)
         containerView.addSubview(contentViewController.view)
         contentViewController.view.snp.makeConstraints { make in
-            switch contentViewController.modalStyle {
-            case .bottomSheet:
-                make.bottom.equalToSuperview().inset(ModalConfig.bottomSheetStyleBottomInset)
-                make.top.equalToSuperview().inset(ModalConfig.containerVerticalContentInset)
-                make.horizontalEdges.equalToSuperview()
-            case .modal:
-                make.verticalEdges.equalToSuperview().inset(ModalConfig.containerVerticalContentInset)
-                make.horizontalEdges.equalToSuperview().inset(ModalConfig.bottomSheetStyleHorizontalInset)
-            case .alert:
-                make.edges.equalToSuperview().inset(ModalConfig.alertSheetStyleInset)
-            }
+            make.bottom.equalToSuperview().inset(ModalConfig.bottomSheetStyleBottomInset)
+            make.top.equalToSuperview().inset(ModalConfig.containerVerticalContentInset)
+            make.horizontalEdges.equalToSuperview()
             if let height = contentViewController.modalHeight { make.height.equalTo(height) }
         }
         contentViewController.didMove(toParent: parent)
     }
 
-    @objc private func handlePan(_ gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: containerView)
-
-        switch gesture.state {
-        case .began:
-            // 초기화 필요 시 여기에
-            break
-
-        case .changed:
-            // 아래로 드래그 할 때만 이동
-            if translation.y > 0 {
-                containerView.transform = CGAffineTransform(translationX: 0, y: translation.y)
-                dimView.alpha = max(0, 1 - (translation.y / 300))
-            }
-
-        case .ended, .cancelled:
-            let velocity = gesture.velocity(in: containerView).y
-            let shouldDismiss = translation.y > 150 || velocity > 1000
-
-            if shouldDismiss {
-                UIView.animate(withDuration: 0.25, animations: {
-                    self.containerView.transform = CGAffineTransform(translationX: 0, y: self.containerView.bounds.height)
-                    self.dimView.alpha = 0
-                }, completion: { _ in
-                    self.parentViewController?.dismissCurrentModal()
-                })
-            } else {
-                // 복귀 애니메이션
-                UIView.animate(withDuration: 0.25, animations: {
-                    self.containerView.transform = .identity
-                    self.dimView.alpha = 1
-                })
-            }
-        default:
-            break
+    func animateDismiss(completion: @escaping () -> Void) {
+        if let bottomConstraint = containerBottomConstraint {
+            // 아래로 내리기 위해 음수 inset
+            bottomConstraint.update(inset: -containerView.bounds.height)
+            UIView.animate(withDuration: 0.35, delay: 0, options: [.curveEaseIn], animations: {
+                self.layoutIfNeeded()
+                self.dimView.alpha = 0
+            }, completion: { _ in
+                completion()
+            })
         }
     }
 
