9f800e5705c0bbbdac0fee3436444fc7adc6c5f1
p2glet <vviian.2@gmail.com>
2025-10-27 15:57:48 +0900

    fix/#258: 몬스터상세 - 출현맵 필터 수정

--- MLS/Domain/Domain/Interceptor/TokenInterceptor.swift
+++ MLS/Domain/Domain/Interceptor/TokenInterceptor.swift
@@ -11,15 +11,19 @@ public class TokenInterceptor: Interceptor {
     }
 
     public func adapt(_ request: URLRequest) -> URLRequest {
-        let accessFetchResult = fetchTokenUseCase.execute(type: .accessToken)
-        switch accessFetchResult {
-        case .success(let token):
-            var adaptedRequest = request
-            adaptedRequest.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-            return adaptedRequest
-        case .failure:
-            return request
-        }
+//        let accessFetchResult = fetchTokenUseCase.execute(type: .accessToken)
+//        switch accessFetchResult {
+//        case .success(let token):
+//            var adaptedRequest = request
+//            adaptedRequest.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+//            return adaptedRequest
+//        case .failure:
+//            return request
+//        }
+        var adaptedRequest = request
+               adaptedRequest.setValue("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0MjU2MDIyNDk4IiwiaWF0IjoxNzU5MjQyOTQ2LCJleHAiOjIwNzQ2MDI5NDZ9.xZQ-lckX6ADalNeMrvez5vzLFSuU1lqPUZZiCYGdX8E", forHTTPHeaderField: "Authorization")
+               return adaptedRequest
+
     }
 
     public func retry(data: Data?, response: URLResponse?, error: Error?) -> Bool {
