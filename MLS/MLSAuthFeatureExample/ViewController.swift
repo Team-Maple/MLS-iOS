a0d791f1565eb3036d64781dc20ed28000be2f3e
JunYoung <ghddns34@gmail.com>
2026-04-08 20:34:49 +0900

    feat/#316: auth feature example 타깃 추가

---
+++ MLS/MLSAuthFeatureExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLSAuthFeatureExample
+//
+//  Created by SeoJunYoung on 4/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
