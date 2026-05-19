3662508e40f97b77c261f8b95af5b8ad41326201
p2glet <vviian.2@gmail.com>
2026-05-19 22:15:58 +0900

    feat/#332: MLSDictionaryFeature 모듈 생성

---
+++ MLS/MLSDictionaryFeature/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MLSDictionaryFeature",
+    platforms: [.iOS(.v15)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MLSDictionaryFeature",
+            targets: ["MLSDictionaryFeature"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "MLSDictionaryFeature"
+        ),
+        .testTarget(
+            name: "MLSDictionaryFeatureTests",
+            dependencies: ["MLSDictionaryFeature"]
+        ),
+    ]
+)
